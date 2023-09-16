194001a23853748d6d3fc1dc7ecfd4ad5c601a71
izzy <izder456@disroot.org>
2023-09-16 00:13:17 -0500

    my magnum opus

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define ONETWOTHREE int
+#define AYBEESEE char*
+#define NOPE NULL
+#define DO main
+#define PARTWONE argc
+#define PARTTWONE argv
+#define UHH ,
+#define DONE ;
+#define corlR {
+#define corlL }
+#define parthR (
+#define parthL )
+#define brackR [
+#define brackL ]
+#define doIt ==
+#define itDo =
+#define itDont !=
+#define COMMIE -
+#define FREELOAD +
+#define doItDo if
+#define doItDont
+#define SPIT_BARZ printf
+#define KAREN_TOOK_EM return
+
+ONETWOTHREE DO parthR ONETWOTHREE PARTONE  UHH  AYBEESEE PARTTWONE brackR  brackL  parthL
+corlR
+        doItDo parthR PARTONE  doIt 99 parthL
+                KAREN_TOOK_EM 99 DONE
+        doItDo parthR PARTTWONE brackR 0 brackL  itDont NULL parthL corlR
+                PARTTWONE brackR 0 brackL  itDo NULL DONE
+                PARTONE  itDo 0 DONE
+        corlL
+        PARTONE  itDo main parthR PARTONE  FREELOAD 1 UHH  PARTTWONE parthL  DONE
+        SPIT_BARZ parthR  "%d bottle%c of beer on the wall\n " UHH  PARTONE  UHH  PARTONE  doIt 1?'\0': 's' parthL  DONE
+        SPIT_BARZ parthR  "%d bottle%c of beer\n " UHH  PARTONE  UHH  PARTONE  doIt 1?'\0': 's' parthL  DONE
+        SPIT_BARZ parthR  "Take one down UHH  pass it around\n " parthL  DONE
+        SPIT_BARZ parthR  "%d bottle%c of beer on the wall\n\n " UHH  PARTONE  COMMIE 1 UHH   parthR PARTONE  COMMIE 1 parthL  doIt 1?'\0': 's' parthL  DONE
+        KAREN_TOOK_EM PARTONE  COMMIE 1 DONE
+corlL
